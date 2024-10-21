aa0516180fc7f68a0bb4ef9dd22b75f83de2a238
Muhammadali <mukhammadaliweb@gmail.com>
2024-10-21 17:13:09 +0500

    Added secret path with uuid on webhook

--- src/app.js
+++ src/app.js
@@ -1,30 +1,34 @@
-require('./index')
+require("./index");
 
-const express = require('express')
-const bot = require('./core/bot')
-const { SERVER_URL, PORT } = require('./config/environments')
+const express = require("express");
+const bot = require("./core/bot");
+const { SERVER_URL, PORT } = require("./config/environments");
+const uuid = require("uuid");
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use('/namoztutor_bot', bot.webhookCallback('/secret-path'))
-bot.telegram.setWebhook(SERVER_URL)
+const secretPath = "/" + uuid.v4();
+const webhookUrl = SERVER_URL + secretPath;
 
-app.get('/namoztutor_bot', (req, res) => {
-    res.status(200).json({
-        ok: true,
-        message: "Bot running"
-    })
-})
+app.use("/namoztutor_bot", bot.webhookCallback(secretPath));
+bot.telegram.setWebhook(webhookUrl);
+
+app.get("/namoztutor_bot", (req, res) => {
+  res.status(200).json({
+    ok: true,
+    message: "Bot running",
+  });
+});
 
 app.use((req, res) => {
-    res.status(404).json({
-        ok: false,
-        message: "Page not found"
-    })
-})
+  res.status(404).json({
+    ok: false,
+    message: "Page not found",
+  });
+});
 
 app.listen(PORT, () => {
-    console.log(`Bot launched on port: ${PORT} ${new Date()}`)
-})
+  console.log(`Bot launched on port: ${PORT} ${new Date()}`);
+});
